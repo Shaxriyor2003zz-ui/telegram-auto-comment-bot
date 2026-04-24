4eadfd26a2873b0f890ac12f95541c2259f5202d
Shaxriyor2003zz-ui <Shaxriyor2003zz@icloud.com>
2026-04-24 13:26:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import os
 
-# ENV o'zgaruvchilar
 TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+OWNER_ID = os.getenv("OWNER_ID")
 
-# Avtomatik yoziladigan komment
+# tekshir
+if not TOKEN:
+    print("❌ BOT_TOKEN topilmadi")
+    exit()
+
+if not OWNER_ID:
+    print("❌ OWNER_ID topilmadi")
+    exit()
+
+try:
+    OWNER_ID = int(OWNER_ID)
+except:
+    print("❌ OWNER_ID noto‘g‘ri formatda")
+    exit()
+
 AUTO_COMMENT = """💎 Premium
 👤 Admin: @RF_shakhr
 📞 Telefon: +998 91 778 26 81
@@ -17,17 +30,15 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.effective_message
     user = update.effective_user
 
-    # faqat o'zing yozgan xabarga ishlaydi
     if msg and user and user.id == OWNER_ID:
         try:
             await msg.reply_text(AUTO_COMMENT)
         except Exception as e:
             print("Xatolik:", e)
 
-# Botni ishga tushirish
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, handle))
 
-print("Bot ishga tushdi...")
+print("✅ Bot ishga tushdi")
 
 app.run_polling()
