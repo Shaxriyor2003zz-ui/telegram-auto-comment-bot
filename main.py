04bbf1b428350a38d45bfb674e380f4cf62c0bd7
Shaxriyor2003zz-ui <Shaxriyor2003zz@icloud.com>
2026-04-24 14:00:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,17 @@ AUTO_COMMENT = """💎 Premium
 Buyurtma uchun admin bilan bog‘laning ✨"""
 
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg = update.effective_message
+    msg = update.message
     user = update.effective_user
 
-    # faqat guruhda ishlaydi
-    if update.effective_chat.type in ["group", "supergroup"]:
-        if user and user.id == OWNER_ID:
+    if msg and user:
+        if user.id == OWNER_ID:
             await msg.reply_text(AUTO_COMMENT)
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-# ❗ MUHIM: text filter qo‘shildi
-app.add_handler(MessageHandler(filters.TEXT, handle))
+# ❗ eng stabil variant
+app.add_handler(MessageHandler(filters.ALL, handle))
 
 print("Bot ishlayapti...")
 
