319602850f2213a2ba2419a67d5de156482416f8
Shaxriyor2003zz-ui <Shaxriyor2003zz@icloud.com>
2026-04-24 13:46:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filte
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = os.getenv("OWNER_ID")
-
-# tekshir
-if not TOKEN:
-    print("❌ BOT_TOKEN topilmadi")
-    exit()
-
-if not OWNER_ID:
-    print("❌ OWNER_ID topilmadi")
-    exit()
-
-try:
-    OWNER_ID = int(OWNER_ID)
-except:
-    print("❌ OWNER_ID noto‘g‘ri formatda")
-    exit()
+OWNER_ID = int(os.getenv("OWNER_ID"))
 
 AUTO_COMMENT = """💎 Premium
 👤 Admin: @RF_shakhr
@@ -30,15 +15,16 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.effective_message
     user = update.effective_user
 
-    if msg and user and user.id == OWNER_ID:
-        try:
+    # faqat guruhda ishlaydi
+    if update.effective_chat.type in ["group", "supergroup"]:
+        if user and user.id == OWNER_ID:
             await msg.reply_text(AUTO_COMMENT)
-        except Exception as e:
-            print("Xatolik:", e)
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.ALL, handle))
 
-print("✅ Bot ishga tushdi")
+# ❗ MUHIM: text filter qo‘shildi
+app.add_handler(MessageHandler(filters.TEXT, handle))
+
+print("Bot ishlayapti...")
 
 app.run_polling()
